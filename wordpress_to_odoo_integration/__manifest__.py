f6199db7f31434e964ded5e58080249aafa4d3cd
austin755 <ashhish.namahsoftech@gmail.com>
2025-12-14 23:21:41 +0530

    [ashish][module name and cache deleted]

--- wordpress_to_odoo_integration/__manifest__.py
+++ wordpress_to_odoo_integration/__manifest__.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Meta To Odoo CRM Integration',
+    'name': 'Wordpress To Odoo Integration',
     'version': '1.0',
     'depends': ['base','crm'],
     'author': 'NSPL',
     'category': 'Sales',
     'description': 'Sales',
     'data':[
-        'security/ir.model.access.csv',
-        'data/fetch_lead_cron.xml',
-        'views/res_config_settings_view.xml',
-		'views/crm_lead_view.xml', 
 	],
     'license':'LGPL-3',
     'installable': True,
